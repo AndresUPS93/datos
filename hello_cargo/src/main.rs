27599de8bdd2fba6841cb2d5b293c0ec16c2a4ba
AndresUPS93 <58879864+AndresUPS93@users.noreply.github.com>
2021-02-01 21:50:18 -0500

    Add files via upload

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+
+use  postgres :: { Conexión , TlsMode };
+let  url  =  "postgresql: // postgres: hunter2 @ localhost: 5432: 2994 / foodb" ;
+let  conn  =  Connection :: connect ( url , TlsMode :: None ). desenvolver ();
+
+
+fn main() {
+
+    
+}
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\Documentos\RUST\hello_cargo\target\debug\hello_cargo.exe: D:\Documentos\RUST\hello_cargo\src\main.rs
